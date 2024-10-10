155e457dcccdbfe70cffa432ace69b1720600c78
MILKv2 <janek.tomkowicz122@gmail.com>
2024-10-10 16:23:51 +0200

    improvements

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Simple tool made in python, to check minecraft servers (beta)
 3. `pip install -r requirements.txt`
 4. `python3 main.py`
 
-Used https://api.mcsrvstat.us/ in this project.
+Used https://api.mcsrvstat.us/ in this project & https://check-host.net/about/api?lang=en check-host api.
 And since im not the best in python, i used some tools like Chatgpt for help.
 
--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import time
 import requests
 import dns.resolver
 import sys
+import socket
 
 init()
 
@@ -81,7 +82,8 @@ def check_server_status(address):
             plugins = [f"{plugin['name']} v{plugin['version']}" for plugin in data.get("plugins", [])] or "Not found"
             info = "\n".join(data.get("info", {}).get("clean", ["Not found"]))
 
-            type_text_with_gradient(f"Players Online: {players_online}/{players_max}", (255, 0, 0), (0, 0, 255), delay=0.02)
+            type_text_with_gradient(f"\n  Server stats", (255, 0, 0), (0, 0, 255), delay=0.01)
+            type_text_with_gradient(f"\nPlayers Online: {players_online}/{players_max}", (255, 0, 0), (0, 0, 255), delay=0.02)
             type_text_with_gradient(f"Port: {port}", (255, 0, 0), (0, 0, 255), delay=0.02)
             type_text_with_gradient(f"IP Address: {ip_address}", (255, 0, 0), (0, 0, 255), delay=0.02)
             type_text_with_gradient(f"Hostname: {hostname}", (255, 0, 0), (0, 0, 255), delay=0.02)
@@ -96,7 +98,7 @@ def check_server_status(address):
             main()
 
         else:
-            print(Fore.RED + "[!] Server is offline." + Style.RESET_ALL)
+            print(Fore.RED + "\n\n[!] Server is offline or I was not able to get status from API." + Style.RESET_ALL)
             main()
             
     except requests.exceptions.HTTPError as http_err:
@@ -106,13 +108,17 @@ def check_server_status(address):
 
 def dns_scan(domain):
     try:
-        type_text_with_gradient(f"Scanning DNS records for {domain}...", (255, 0, 0), (0, 0, 255), delay=0.02)
+        type_text_with_gradient(f"\nScanning DNS records for {domain}...", (255, 0, 0), (0, 0, 255), delay=0.02)
         time.sleep(1)
 
-        a_records = dns.resolver.resolve(domain, 'A')
-        type_text_with_gradient("A Records:", (0, 255, 0), (0, 0, 255), delay=0.02)
-        for record in a_records:
-            type_text_with_gradient(f" - {record.to_text()}", (0, 255, 0), (0, 0, 255), delay=0.02)
+        try:
+            a_records = dns.resolver.resolve(domain, 'A')
+            type_text_with_gradient("A Records:", (0, 255, 0), (0, 0, 255), delay=0.02)
+            for record in a_records:
+                type_text_with_gradient(f" - {record.to_text()}", (0, 255, 0), (0, 0, 255), delay=0.02)
+
+        except dns.resolver.NoAnswer:
+            type_text_with_gradient("A Records: Not found", (0, 255, 0), (0, 0, 255), delay=0.02)
 
         try:
             aaaa_records = dns.resolver.resolve(domain, 'AAAA')
@@ -122,48 +128,113 @@ def dns_scan(domain):
         except dns.resolver.NoAnswer:
             type_text_with_gradient("AAAA Records: Not found", (0, 255, 0), (0, 0, 255), delay=0.02)
 
-        mx_records = dns.resolver.resolve(domain, 'MX')
-        type_text_with_gradient("MX Records:", (0, 255, 0), (0, 0, 255), delay=0.02)
-        for record in mx_records:
-            type_text_with_gradient(f" - {record.exchange.to_text()} (Preference: {record.preference})", (0, 255, 0), (0, 0, 255), delay=0.02)
+        try:
+            mx_records = dns.resolver.resolve(domain, 'MX')
+            type_text_with_gradient("MX Records:", (0, 255, 0), (0, 0, 255), delay=0.02)
+            for record in mx_records:
+                type_text_with_gradient(f" - {record.exchange.to_text()} (Preference: {record.preference})", (0, 255, 0), (0, 0, 255), delay=0.02)
+        except dns.resolver.NoAnswer:
+            type_text_with_gradient("MX Records: Not found", (0, 255, 0), (0, 0, 255), delay=0.02)
+
+        try:
+            srv_records = dns.resolver.resolve(domain, 'SRV')
+            type_text_with_gradient("SRV Records:", (0, 255, 0), (0, 0, 255), delay=0.02)
+            for record in srv_records:
+                type_text_with_gradient(f" - {record.target.to_text()}:{record.port} (Priority: {record.priority}, Weight: {record.weight})", (0, 255, 0), (0, 0, 255), delay=0.02)
+        except dns.resolver.NoAnswer:
+            type_text_with_gradient("SRV Records: Not found", (0, 255, 0), (0, 0, 255), delay=0.02)
 
     except dns.resolver.NoAnswer:
         print(Fore.RED + "[!] This domain/ip does not exist, or I was not able to find it." + Style.RESET_ALL)
     except dns.resolver.Timeout:
         print(Fore.RED + "[!] DNS query timed out." + Style.RESET_ALL)
-    except Exception as e:
-        print(Fore.RED + f"[!] An error occurred during DNS scan: {str(e)}" + Style.RESET_ALL)
 
-def full_scan():
-    start_color = (255, 0, 0)  
-    end_color = (0, 0, 255)    
-    rgb_gradient("[!] Server Domain/IP: ", start_color, end_color)  
-    ip = input("").strip()
+def check_host(address):
+    max_nodes = 3
+    check_types = ['ping', 'http', 'tcp', 'dns', 'udp']
+    
+    request_ids = {}
+    results = {}
 
-    if not all(part.isdigit() for part in ip.split(".")) or len(ip.split(".")) != 4:
-        dns_scan(ip)  
-    else:
-        print(Fore.YELLOW + "[*] Input is an IP address; skipping DNS scan." + Style.RESET_ALL)
+    for check_type in check_types:
+        url = f"https://check-host.net/check-{check_type}?host={address}&max_nodes={max_nodes}"
+        
+        try:
+            response = requests.get(url, headers={"Accept": "application/json"})
+            response.raise_for_status()
+            data = response.json()
+            if data.get("ok") == 1:
+                request_id = data['request_id']
+                request_ids[check_type] = request_id
+            else:
+                print(Fore.RED + f"[!] Failed to initiate {check_type} check." + Style.RESET_ALL)
 
-    type_text_with_gradient("Connecting to api..", start_color, end_color, delay=0.05)
-    time.sleep(1)
-    check_server_status(ip) 
+        except requests.exceptions.RequestException as e:
+            print(Fore.RED + f"[!] An error occurred while checking {check_type}: " + str(e) + Style.RESET_ALL)
+
+    for check_type, request_id in request_ids.items():
+        result_url = f"https://check-host.net/check-result/{request_id}"
+        try:
+            result_response = requests.get(result_url, headers={"Accept": "application/json"})
+            result_response.raise_for_status()
+            results[check_type] = result_response.json()
+        
+        except requests.exceptions.RequestException as e:
+            print(Fore.RED + f"[!] An error occurred while fetching results for {check_type}: " + str(e) + Style.RESET_ALL)
+
+    for check_type, result in results.items():
+        type_text_with_gradient(f"\nResults for {check_type} check:", (255, 0, 0), (0, 0, 255), delay=0.02)
+        for node, checks in result.items():
+            type_text_with_gradient(f"Node: {node}", (0, 255, 0), (0, 0, 255), delay=0.02)
+            if checks is None:
+                print(Fore.RED + "  No results available." + Style.RESET_ALL)
+                continue
+
+            if check_type == 'ping':
+                for check in checks:
+                    type_text_with_gradient(Fore.RED + f"  Status: {check[0]}, Time: {check[1]} seconds, IP: {check[2]}", (0, 255, 0), (0, 0, 255), delay=0.02)
+            elif check_type == 'http':
+                for check in checks:
+                    type_text_with_gradient(Fore.LIGHTCYAN_EX + f"  Status: {check[2]} {check[3]}, IP: {check[4]}, Time: {check[1]} seconds", (0, 255, 0), (0, 0, 255), delay=0.02)
+            elif check_type == 'tcp':
+                for check in checks:
+                    if "error" in check:
+                        print(Fore.RED + f"  Error: {check['error']}" + Style.RESET_ALL)
+                    else:
+                        type_text_with_gradient(Fore.LIGHTCYAN_EX + f"  Connected to IP: {check['address']}, Time: {check['time']} seconds", (0, 255, 0), (0, 0, 255), delay=0.02)
+            elif check_type == 'dns':
+                for check in checks:
+                    for record_type, values in check.items():
+                        if isinstance(values, list):  
+                            type_text_with_gradient(Fore.LIGHTCYAN_EX + f"  {record_type}: {', '.join(values)}", (0, 255, 0), (0, 0, 255), delay=0.02)
+                        else:
+                            type_text_with_gradient(Fore.LIGHTCYAN_EX + f"  {record_type}: {values}", (0, 255, 0), (0, 0, 255), delay=0.02)
+            elif check_type == 'udp':
+                print(Fore.RED + "UDP checks are not implemented (or some error?)." + Style.RESET_ALL)
+
+
+
 
 def main():
-    start_color = (255, 0, 0)  
-    end_color = (0, 0, 255)    
+    while True:
+        type_text_with_gradient("\n\nWelcome to the Minecraft Server Checker!\n", (255, 0, 0), (0, 0, 255), delay=0.02)
+        type_text_with_gradient("[1] Full Scan", (255, 0, 0), (0, 0, 255), delay=0.02)
+        type_text_with_gradient("[2] Exit", (255, 0, 0), (0, 0, 255), delay=0.02)
 
-    print(Fore.GREEN + "\n\nWelcome, choose an option to continue:\n" + Style.RESET_ALL)
-    rgb_gradient("[1] Full scan\n", start_color, end_color)
-    rgb_gradient("[2] Exit\n", start_color, end_color)
-    option = int(input(Fore.GREEN + "Your option: " + Style.RESET_ALL))
-
-    if option == 1:
-        full_scan()
-    elif option == 2:
-        sys.exit()
-    else:
-        print(Fore.RED + "\nChoose valid option.\n" + Style.RESET_ALL)
+        choice = input(Fore.GREEN + "Choose an option: " + Style.RESET_ALL)
+        
+        if choice == "1":
+            ip = input(Fore.CYAN + "Enter the Minecraft server address (domain or IP): " + Style.RESET_ALL)
+            dns_scan(ip)
+            check_host(ip) 
+            check_server_status(ip)
+        
+        elif choice == "2":
+            type_text_with_gradient("See You next time..", (255, 0, 0), (0, 0, 255), delay=0.02)
+            sys.exit()
+        
+        else:
+            print(Fore.RED + "[!] Invalid option." + Style.RESET_ALL)
 
 if __name__ == "__main__":
     main()
